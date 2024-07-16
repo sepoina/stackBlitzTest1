451ce73009e49e8f0d69d45963408640aa119f65
sepoina <sepoina@gmail.com>
2024-07-16 14:09:00 +0200

    aggiornamento eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import useAction from './useAction';
-import avatarActionFunction from './avatarActionFunction';
+import useAction from "./useAction";
+import avatarActionFunction from "./avatarActionFunction";
 
 // Esempio di utilizzo del hook
 export default function App() {
+
   const Avatar = useAction(avatarActionFunction, {
     avatar: null,
-    status: 'booting',
+    status: "booting",
   });
 
   const handleLoad = () => {
-    if (Avatar.state.status !== 'onLoading') {
-      Avatar.action('load', parseInt(Math.random() * 15).toString());
+    if (Avatar.state.status !== "onLoading") {
+     Avatar.action("load", parseInt(Math.random() * 15).toString());
     }
   };
 
   return (
     <div>
       <p>Loading state: {Avatar.state.status}</p>
-      {Avatar.state.status === 'okData' && (
+      {Avatar.state.status === "okData" && (
         <>
           <pre>{JSON.stringify(Avatar.state.data, null, 4)}</pre>
           <img src={Avatar.state.data.avatar} />
         </>
       )}
-      {Avatar.state.status === 'error' && (
+      {Avatar.state.status === "error" && (
         <>
           <div>{JSON.stringify(Avatar.state.data, null, 4)}</div>
         </>
@@ -32,7 +33,7 @@ export default function App() {
       <br />
       <button
         onClick={handleLoad}
-        disabled={Avatar.state.status === 'onLoading'}
+        disabled={Avatar.state.status === "onLoading"}
       >
         Load Avatar
       </button>
